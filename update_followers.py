848e03c211aeca8f6de259aa3c7451342c9792c9
Julien-Quinodoz <julien.quinodoz@gmail.com>
2025-02-21 14:07:24 +0100

    README.md

--- update_followers.py
+++ update_followers.py
@@ -1,3 +1,4 @@
+
 import requests
 import os
 
@@ -18,7 +19,7 @@ def update_followers():
 
     followers = response.json()[::-1]  # Inverser pour afficher les derniers en premier
     followers_list = "\n".join([
-        f"{i+1}. ![Avatar](https://github.com/{follower['login']}.png?s=30) [{follower['login']}]({follower['html_url']})"
+        f"{i+1}. ![Avatar](https://github.com/{follower['login']}.png?s=10) [{follower['login']}]({follower['html_url']})"
         for i, follower in enumerate(followers)
     ])
 
